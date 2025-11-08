8568acb3d9b3c7a059c45b251a6c429656bbbe34
pheonixeye <drkazgm@gmail.com>
2025-11-08 23:03:46 +0200

    working on notifications ui - added pop over package - to work in creating in-clinic notification and add them to favorites for each device

--- lib/pages/loading_page/pages/lang_page/pages/shell_page/widgets/nav_bar.dart
+++ lib/pages/loading_page/pages/lang_page/pages/shell_page/widgets/nav_bar.dart
@@ -10,10 +10,14 @@ import 'package:allevia_one/assets/assets.dart';
 import 'package:allevia_one/extensions/is_mobile_context.dart';
 import 'package:allevia_one/providers/px_locale.dart';
 import 'package:allevia_one/router/router.dart';
+import 'package:popover/popover.dart';
 import 'package:provider/provider.dart';
 
 class Navbar extends StatelessWidget implements PreferredSizeWidget {
   const Navbar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
 
   @override
   Widget build(BuildContext context) {
@@ -62,60 +66,114 @@ class Navbar extends StatelessWidget implements PreferredSizeWidget {
                   ),
                 ),
                 const Spacer(),
-                Consumer<PxWhatsapp>(
-                  builder: (context, w, _) {
-                    while (w.serverResult == null) {
-                      return const SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    return IconButton.outlined(
-                      tooltip: w.isConnectedToServer
-                          ? '${context.loc.conntectedToWhatsappServer} \n ${w.hasConnectedDevices ? w.connectedDevices?.results?.map((e) => '${e.device}\n').toList() : context.loc.noConnectedDevices}'
-                          : '${context.loc.notConntectedToWhatsappServer} \n ${context.loc.noConnectedDevices}',
-                      style: IconButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        side: const BorderSide(color: Colors.white),
-                      ),
-                      onPressed: null,
-                      icon: Icon(
-                        w.isConnectedToServer
-                            ? FontAwesomeIcons.whatsapp
-                            : Icons.wifi_off_rounded,
-                        color:
-                            w.isConnectedToServer ? Colors.green : Colors.red,
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(width: 20),
                 IconButton.outlined(
-                  tooltip: context.loc.visitsCalender,
                   style: IconButton.styleFrom(
                     foregroundColor: Colors.white,
                     side: const BorderSide(color: Colors.white),
                   ),
-                  onPressed: () async {
-                    await showGeneralDialog<void>(
+                  onPressed: () {
+                    showPopover(
                       context: context,
-                      pageBuilder: (context, a1, a2) {
-                        return MonthlyVisitsCalendarDialog();
-                      },
-                      anchorPoint: Offset(100, 100),
-                      transitionDuration: const Duration(milliseconds: 600),
-                      transitionBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        return ScaleTransition(
-                          alignment: Alignment.topLeft,
-                          scale: animation,
-                          child: child,
+                      // onPop: () => print('Popover was popped!'),
+                      direction: PopoverDirection.bottom,
+                      backgroundColor: Colors.blue.shade50,
+                      width: 200,
+                      height: 200,
+                      arrowHeight: 15,
+                      arrowWidth: 30,
+                      bodyBuilder: (context) {
+                        return ListView(
+                          padding: const EdgeInsets.all(8),
+                          children: [
+                            Consumer<PxWhatsapp>(
+                              builder: (context, w, _) {
+                                while (w.serverResult == null) {
+                                  return const SizedBox(
+                                    width: 30,
+                                    height: 8,
+                                    child: LinearProgressIndicator(),
+                                  );
+                                }
+                                return ListTile(
+                                  titleAlignment: ListTileTitleAlignment.top,
+                                  leading: Icon(
+                                    w.isConnectedToServer
+                                        ? FontAwesomeIcons.whatsapp
+                                        : Icons.wifi_off_rounded,
+                                    color: w.isConnectedToServer
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                  title: Text(context.loc.whatsappSettings),
+                                  subtitle: Row(
+                                    spacing: 4,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(w.isConnectedToServer
+                                          ? '${context.loc.conntectedToWhatsappServer} \n ${w.hasConnectedDevices ? w.connectedDevices?.results?.map((e) => '${e.device}\n').toList() : context.loc.noConnectedDevices}'
+                                          : '${context.loc.notConntectedToWhatsappServer} \n ${context.loc.noConnectedDevices}'),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                            const Divider(),
+                            ListTile(
+                              leading: const Icon(Icons.calendar_month),
+                              title: Text(context.loc.visitsCalender),
+                              titleAlignment: ListTileTitleAlignment.top,
+                              onTap: () async {
+                                await showGeneralDialog<void>(
+                                  context: context,
+                                  pageBuilder: (context, a1, a2) {
+                                    return MonthlyVisitsCalendarDialog();
+                                  },
+                                  anchorPoint: Offset(100, 100),
+                                  transitionDuration:
+                                      const Duration(milliseconds: 600),
+                                  transitionBuilder: (context, animation,
+                                      secondaryAnimation, child) {
+                                    return ScaleTransition(
+                                      alignment: Alignment.topLeft,
+                                      scale: animation,
+                                      child: child,
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                            const Divider(),
+                            ListTile(
+                              titleAlignment: ListTileTitleAlignment.top,
+                              leading: const Icon(Icons.notification_add),
+                              title: Text(context.loc.notifications),
+                              onTap: () {
+                                showPopover(
+                                  context: context,
+                                  backgroundColor: Colors.amber.shade50,
+                                  direction: l.isEnglish
+                                      ? PopoverDirection.right
+                                      : PopoverDirection.left,
+                                  width: 150,
+                                  height: 200,
+                                  arrowHeight: 15,
+                                  arrowWidth: 30,
+                                  bodyBuilder: (context) {
+                                    return ListView(
+                                      children: [
+                                        //TODO: add clinic calls + clinic calls dialog
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ],
                         );
                       },
                     );
                   },
-                  icon: const Icon(Icons.calendar_month),
+                  icon: const Icon(Icons.notification_add),
                 ),
                 const SizedBox(width: 10),
               ],
@@ -142,7 +200,4 @@ class Navbar extends StatelessWidget implements PreferredSizeWidget {
       },
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(60);
 }
